2a3d8a51fd3aa234d006421945bb1defd64bf88b
nuriaktas <aktasnuri88@gmail.com>
2024-12-20 00:10:16 +0300

    commit -m Merge branch 'main' of https://github.com/nourish88/next-match

--- src/lib/schemas/Products/productSchema.ts
+++ src/lib/schemas/Products/productSchema.ts
@@ -2,11 +2,7 @@ import { z } from "zod";
 
 export const productSchema = z.object({
   name: z.string().min(3),
-<<<<<<< HEAD
   barcode: z.string().optional(),
-=======
-  barcode: z.string().optinal(),
->>>>>>> 710ce76c3b25dc3c983afb91834a032204803ad1
   groupId: z.number().max(1000).int().positive(),
 });
 
