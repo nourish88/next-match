b10fa3ecfe395c3147fd9cf102406f3b48ded23d
nuriaktas <aktasnuri88@gmail.com>
2024-12-20 16:41:10 +0300

    phoneNumber not required

--- src/lib/schemas/Customers/customerSchema.ts
+++ src/lib/schemas/Customers/customerSchema.ts
@@ -1,14 +1,12 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const phoneRegex = new RegExp(
-    /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
-  );
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
 export const customerSchema = z.object({
-    name: z.string().min(3),
-    surName: z.string().min(3),
-    phoneNumber: z.string().regex(phoneRegex, 'Invalid Number!'),
-    
-})
+  name: z.string().min(3),
+  surName: z.string().min(3),
+  phoneNumber: z.string().regex(phoneRegex, "Invalid Number!").optional(),
+});
 
-export type CustomerSchema = z.infer<typeof customerSchema>
-
+export type CustomerSchema = z.infer<typeof customerSchema>;
